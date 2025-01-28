d9caac0c7345af3f8a38fe87dbd957649d0b7b8d
David Gordon <david.gordon@kellogg.northwestern.edu>
2025-01-28 21:40:02 +0000

    end of code along

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -4,8 +4,28 @@
 
 # Infinite Tacos -> Finite Tacos
 # loop do
-#   puts "tacos!"
-# end
+#     if index == 5
+#     end
+
+#    puts "tacos!"
+
+#     index = index + 1
+
+#  end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+index = 0
+
+loop do
+    if index == tacos.size
+        break
+    end
+    taco = tacos[index]
+
+    puts "#{taco} tacos!"
+
+    index=index+1
+end
